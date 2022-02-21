05988c8ccab259d5c53908724aa54239c0d774d4
khoi anh <khoivva.tdsoftware@gmail.com>
2022-02-21 11:47:51 +0700

    fix bugs

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
 import React, {useCallback, useRef, useState} from 'react';
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {SafeAreaInsetsContext} from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
 import Video, {LoadError, OnBufferData} from 'react-native-video';
 import Spinner from 'react-native-loading-spinner-overlay';
+import images from './assets/images';
 
 type VideoItem = {
   id: number;
@@ -58,16 +67,19 @@ const carouselItems: VideoItem[] = [
     url: 'https://aiapoc-aase.streaming.media.azure.net/ab4a57df-80bb-46ec-b3cb-55664ac57329/VID_20220208_161937.ism/manifest',
   },
 ];
+const tmp = Dimensions.get('screen');
+const {width, height} = tmp;
 
 const App = () => {
   const videoPlayer = useRef<Video>(null);
   const carousel = useRef<Carousel<string>>(null);
-  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const [playIndex, setPlayIndex] = useState<number>(0);
   const [isLoadingIndex, setIdLoadingIndex] = useState<boolean[]>([]);
+  const [reloadIndex, setReloadIndex] = useState<number>(-1);
   const [errors, setErrors] = useState<boolean[]>([]);
 
-  const onBuffer = (e: OnBufferData) => {
-    console.log('onBuffer', e);
+  const onBuffer = (e: OnBufferData, index: number) => {
+    console.log('onBuffer -> index', index, e);
   };
   const videoError = useCallback(
     (e: LoadError, index: number) => {
@@ -79,23 +91,30 @@ const App = () => {
     [errors],
   );
 
-  const handleProgress = useCallback(data => {
-    console.log('handleProgress -> data', data);
+  const handleProgress = useCallback((data, index) => {
+    console.log('handleProgress -> index', index, data);
+    if (
+      data.playableDuration === 0 &&
+      data.currentTime > 0 &&
+      data.currentTime < data.seekableDuration
+    ) {
+      console.log('handleProgress -> error');
+      setReloadIndex(index);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleOnload = useCallback(
-    (data, index) => {
-      console.log('handleOnload -> data', data);
-      const cloneIsLoadingIndex = [...isLoadingIndex];
-      cloneIsLoadingIndex[index] = false;
-      setIdLoadingIndex(cloneIsLoadingIndex);
-    },
-    [isLoadingIndex],
-  );
+  const handleOnload = useCallback((data, index) => {
+    console.log('handleOnload -> data', data);
+    const cloneIsLoadingIndex = [...isLoadingIndex];
+    cloneIsLoadingIndex[index] = false;
+    setIdLoadingIndex(cloneIsLoadingIndex);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleOnloadStart = useCallback(
     (index: number) => {
-      console.log('handleOnloadStart -> index', index);
+      console.log('handleOnloadStart -> index', index, isLoadingIndex);
       const cloneIsLoadingIndex = [...isLoadingIndex];
       cloneIsLoadingIndex[index] = true;
       setIdLoadingIndex(cloneIsLoadingIndex);
@@ -107,42 +126,36 @@ const App = () => {
     console.log('handleOnloadEnd -> data');
   }, []);
 
-  const tmp = Dimensions.get('window');
-  const {width, height} = tmp;
+  const onVideoPlayPauseClicked = (index: number) => {
+    console.log('onVideoPlayPauseClicked', index, playIndex);
+    setPlayIndex(playIndex === index ? -1 : index);
+  };
+
   const renderItem = ({item, index}) => {
     const isLoading = isLoadingIndex[index];
     const isError = errors[index];
+    const isReload = reloadIndex === index;
+    const isPause = playIndex !== index;
 
-    console.log(
-      'renderItem -> index',
-      index,
-      isLoading,
-      isError,
-      isLoading && !isError,
-    );
+    console.log('render item', reloadIndex, playIndex);
 
     return (
       <View style={{flex: 1}}>
-        <Spinner
-          visible={isLoading && !isError}
-          textContent={'Loading...'}
-          textStyle={styles.spinnerTextStyle}
-        />
         <Video
-          key={item.id}
+          key={isReload ? `${item.id}-reloaded` : item.id}
           source={{
             uri: item.url,
           }}
           ref={videoPlayer}
-          onBuffer={onBuffer}
+          onBuffer={e => onBuffer(e, index)}
           onError={data => videoError(data, index)}
           style={styles.mediaPlayer}
           resizeMode="contain"
           automaticallyWaitsToMinimizeStalling={false}
-          paused={index !== selectedIndex}
+          paused={isPause}
           onEnd={handleOnloadEnd}
           repeat={true}
-          onProgress={handleProgress}
+          onProgress={data => handleProgress(data, index)}
           onLoad={data => handleOnload(data, index)}
           onLoadStart={() => handleOnloadStart(index)}
         />
@@ -151,11 +164,31 @@ const App = () => {
             <Text style={styles.errorText}>Cannot load this video</Text>
           </View>
         )}
+        <Spinner
+          visible={(isLoading && !isError) || isReload}
+          textContent={'Loading...'}
+          textStyle={styles.spinnerTextStyle}
+        />
+        <TouchableOpacity
+          onPress={() => onVideoPlayPauseClicked(index)}
+          style={[
+            Platform.OS === 'ios'
+              ? styles.viewBtnPlay
+              : styles.viewBtnPlayAndroid,
+          ]}>
+          {isPause ? (
+            <Image
+              source={images.playBtn}
+              resizeMode="contain"
+              style={styles.iconPlay}
+            />
+          ) : (
+            <View />
+          )}
+        </TouchableOpacity>
       </View>
     );
   };
-
-  console.log('selected index', selectedIndex);
 
   return (
     <SafeAreaInsetsContext.Consumer>
@@ -176,9 +209,11 @@ const App = () => {
               itemWidth={width}
               vertical={true}
               renderItem={renderItem}
-              onSnapToItem={setSelectedIndex}
+              onSnapToItem={setPlayIndex}
               enableMomentum={true}
-              decelerationRate={0.9}
+              lockScrollWhileSnapping={true}
+              inactiveSlideOpacity={1}
+              inactiveSlideScale={1}
             />
           </View>
         );
@@ -195,16 +230,17 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
   },
   mediaPlayer: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: '#333',
+  },
+  safeArea: {flex: 1, backgroundColor: '#333'},
+  viewError: {
     position: 'absolute',
     top: 0,
     left: 0,
     bottom: 0,
     right: 0,
-    backgroundColor: '#333',
-  },
-  safeArea: {flex: 1, backgroundColor: '#333'},
-  viewError: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
   },
@@ -214,6 +250,36 @@ const styles = StyleSheet.create({
   },
   spinnerTextStyle: {
     color: '#FFF',
+  },
+  iconPlay: {
+    width: 50,
+    height: 50,
+    opacity: 0.7,
+  },
+  btnPlay: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  viewBtnPlay: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    // top: 0,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'transparent',
+  },
+  viewBtnPlayAndroid: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 0,
+    bottom: 0,
+    left: 0,
+    top: 0,
+    position: 'absolute',
   },
 });
 
